1620629f42c06e561cce5ab0187035f240593c56
Siying Wu <wusiyi@sheridancollege.ca>
2021-12-16 13:27:24 -0500

    Part 2, class JobVacancy  basic method compelete

--- jobVacancy.py
+++ jobVacancy.py
@@ -3,11 +3,30 @@ Implements the general (base) class for a job vacancy
 """
 
 
+from typing import Text
+
+
 class JobVacancy:
     def __init__(self, jobCode, position, mininumQualifications, salaryOffered):
         self._jobCode = jobCode
         self._position = position
-        self._mininumQualifications = mininumQualifications
+        self._mininumQualifications = self.setMininumQualifications(mininumQualifications)
         self._salaryOffered = salaryOffered
 
-    # Update the class definition as per the instructions in the PDF
+    #create accessor for the jobCode
+    def getJobCode(self):
+        return self._jobCode
+
+    #define mutator of mininumQualifications
+    def setMininumQualifications(self, newMininumQualifications):
+        if newMininumQualifications.lower() == "diploma" or newMininumQualifications.lower() == "bachelors" or newMininumQualifications.lower() == "masters" or newMininumQualifications.lower() == "doctorate":
+            self._mininumQualifications = newMininumQualifications
+        else:
+            raise ValueError ("The input mininum qualifications is not one of diploma, bachelors, masters, or doctorate")
+
+    #override __str__ method of instance
+    def __str__(self):
+        return f"{self.getJobCode},{self._position},{self._mininumQualifications},{self._salaryOffered}"
+
+
+   # Update the class definition as per the instructions in the PDF
