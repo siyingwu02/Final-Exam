a83d782eb9ff4f3ac8ddab982d2dcdc1fe723ea4
Siying Wu <wusiyi@sheridancollege.ca>
2021-12-16 13:32:08 -0500

    Entity Classes Created

--- contractJobVacancy.py
+++ contractJobVacancy.py
@@ -6,5 +6,11 @@ import jobVacancy
 class ContractJobVacancy(jobVacancy.JobVacancy):
     def __init__(self, jobCode, position, mininumQualifications, salaryOffered):
         super().__init__(jobCode, position, mininumQualifications, salaryOffered)
+        self._contractDuration = 12
+
+    #override __str__ method by call base class __str__
+    def __str__(self):
+        return f"{super().__str__()},{self._contractDuration}"
+
     # Update the class definition as per the instructions in the PDF
     pass
